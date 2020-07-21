901c6a6ccb87276fad7faebc9758b3ac593a1831
Julia Shybunka <julishybunka@gmail.com>
2020-07-21 16:07:31 +0300

    Updates

---
+++ precache-manifest.7ec2c74368a5740198303c984fcb5579.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414222424441e262fc79a35ae9386740",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "47455458bc05bc90d93b",
+    "url": "/keeper-app/static/js/2.fb5c3dbf.chunk.js"
+  },
+  {
+    "revision": "9c9ac18ced4f6e568af3",
+    "url": "/keeper-app/static/js/main.5baf8f84.chunk.js"
+  },
+  {
+    "revision": "78797e502974e5d55e3c",
+    "url": "/keeper-app/static/js/runtime-main.e57cdbd2.js"
+  }
+]);
