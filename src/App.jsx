a995f79ff385af15f71a1e230c02e484add5213a
Julia Shybunka <julishybunka@gmail.com>
2020-07-21 13:15:57 +0300

    Implement the add note functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(item){
+    setItems((prevItem)=>{
+      return [item,...prevItem]
+    })
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea addItem={addItem}/>
+      {items.map((item,index)=><Note title={item.title} content={item.content} key={index}/>)}
       <Footer />
     </div>
   );
--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function CreateArea(){
-    return (
-        <div>
-          <form>
-            <input name="title" placeholder="Title" autoComplete="off" />
-            <textarea name="content" placeholder="Take a note..." rows="3" />
-            <button>Add</button>
-          </form>
-        </div>
-      );
+function CreateArea(props) {
+  const [item, setItem] = useState("");
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setItem((prevItem) => {
+      return {
+        ...prevItem,
+        [name]: value,
+      };
+    });
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          autoComplete="off"
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button
+          onClick={(event) => {
+            props.addItem(item);
+            setItem("");
+            event.preventDefault();
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateArea;
